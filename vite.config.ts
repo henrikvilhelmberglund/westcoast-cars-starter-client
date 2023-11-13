e524126eda55b416480981ff8dc3a6b37ee4952b
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-13 15:49:37 +0100

    feat: use reactClickToComponent

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { reactClickToComponent } from "vite-plugin-react-click-to-component";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), reactClickToComponent()],
 })
